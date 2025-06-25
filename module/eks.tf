f4a8cf8698d7461de8fbe8320e347156b4529ef8
rohnny80 <rohnny80@gmail.com>
2025-06-25 14:33:21 -0500

    updated file

--- module/eks.tf
+++ module/eks.tf
@@ -34,10 +34,9 @@ resource "aws_iam_openid_connect_provider" "eks-oidc" {
 
 # AddOns for EKS Cluster
 resource "aws_eks_addon" "eks-addons" {
-  for_each      = { for idx, addon in var.addons : idx => addon }
-  cluster_name  = aws_eks_cluster.eks[0].name
-  addon_name    = each.value.name
-  addon_version = each.value.version
+  cluster_name      = aws_eks_cluster.this.name
+  addon_name        = "coredns"
+  addon_version     = "v1.11.0-eksbuild.2"  # ← replace with supported version
 
   depends_on = [
     aws_eks_node_group.ondemand-node,
